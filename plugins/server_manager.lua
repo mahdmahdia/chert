4a10edaf1dbcf7946568fe9fd48a157d819adf16
mahdmahdia <mahdi028c@gmail.com>
2016-03-01 19:41:58 +0330

    Update server_manager.lua

--- plugins/server_manager.lua
+++ plugins/server_manager.lua
@@ -11,13 +11,9 @@ function run(msg, matches)
 end
 return {
   patterns = {
-    "^[/!](reboot)",
+    "^[reboot]",
     "^[/!](serverinfo)"
   },
   run = run
 }
 end
---Copyright; @behroozyaghi
---Persian Translate; @behroozyaghi
---ch : @nod32team
---کپی بدون ذکر منبع حرام است
