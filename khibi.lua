466b5313d7095c8eb2188109c2abe2219da77203
mahdmahdia <mahdi028c@gmail.com>
2016-03-01 19:32:18 +0330

    Create khibi.lua

---
+++ khibi.lua
@@ -0,0 +1,18 @@
+do
+
+function run(msg, matches)
+  return " مرسی خوبم "
+  end
+return {
+  description = "poker!", 
+  usage = "fuck : ",
+  patterns = {
+    "^خوبم خوبی",
+    "^دوبی",
+    "^خوبی",
+   "^خوبین",
+    "^>دوبی",
+  },
+  run = run
+}
+end
