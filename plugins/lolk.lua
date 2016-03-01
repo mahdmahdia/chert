908e6dd47c7ff4821225d44d70e8b5eea208847a
mahdmahdia <mahdi028c@gmail.com>
2016-03-01 19:26:41 +0330

    Create lolk.lua

---
+++ plugins/lolk.lua
@@ -0,0 +1,18 @@
+do
+
+function run(msg, matches)
+  return " 😃 "
+  end
+return {
+  description = "poker!", 
+  usage = "fuck : ",
+  patterns = {
+    "^😂😂",
+    "^😃",
+    "^😂😂😂",
+   "^😂😂😂😂",
+    "^>😂😂😂😂😂😂",
+  },
+  run = run
+}
+end
