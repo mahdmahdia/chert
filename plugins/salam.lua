41c142b8a1cd9879a99204cbc14b9ac3d81cc511
mahdmahdia <mahdi028c@gmail.com>
2016-03-01 19:29:24 +0330

    Create salam.lua

---
+++ plugins/salam.lua
@@ -0,0 +1,17 @@
+do
+
+function run(msg, matches)
+  return " سلام خوبی؟ "
+  end
+return {
+  description = "poker!", 
+  usage = "fuck : ",
+  patterns = {
+    "^salam",
+    "^slm",
+    "^سلام",
+   "^دلام",
+  },
+  run = run
+}
+end
